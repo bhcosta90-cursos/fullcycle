198c92311310736879e32377e593df331cae959d
Bruno Costa <bhcosta90@gmail.com>
2024-11-02 11:47:15 -0300

    wip

--- src/Core/Domain/Repository/CategoryRepository.php
+++ src/Core/Domain/Repository/CategoryRepository.php
@@ -4,13 +4,8 @@ declare(strict_types = 1);
 
 namespace Package\Core\Domain\Repository;
 
-use Package\Core\Domain\Entity\CategoryEntity;
+use Package\Shared\Domain\Repository\EntityInterface;
 
-interface CategoryRepository
+interface CategoryRepository extends EntityInterface
 {
-    public function create(CategoryEntity $categoryEntity): CategoryEntity;
-
-    public function update(CategoryEntity $categoryEntity): CategoryEntity;
-
-    public function find(string $id): ?CategoryEntity;
 }
---
+++ src/Shared/Domain/Repository/EntityInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Package\Shared\Domain\Repository;
+
+use Package\Shared\Domain\Entity\Entity;
+
+interface EntityInterface
+{
+    public function create(Entity $categoryEntity): Entity;
+
+    public function update(Entity $categoryEntity): Entity;
+
+    public function find(string $id): ?Entity;
+}
