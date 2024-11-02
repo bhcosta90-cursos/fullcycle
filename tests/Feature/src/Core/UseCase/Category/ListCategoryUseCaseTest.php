4dc7f8a173e2fef537d5525f6f5154ab118aca3a
Bruno Costa <bhcosta90@gmail.com>
2024-11-02 14:45:24 -0300

    wip

--- tests/Feature/src/Core/UseCase/Category/ListCategoryUseCaseTest.php
+++ tests/Feature/src/Core/UseCase/Category/ListCategoryUseCaseTest.php
@@ -6,7 +6,7 @@ use App\Models\Category;
 use Package\Application\Query\Category\PaginationCategoryQuery;
 use Package\Core\UseCase\Category\ListCategoryUseCase;
 
-it('creates a category and verifies it in the database', function () {
+it('lists categories and verifies the total count in the database', function () {
     // Arrange
     Category::factory(5)->create();
     $repository = new PaginationCategoryQuery();
