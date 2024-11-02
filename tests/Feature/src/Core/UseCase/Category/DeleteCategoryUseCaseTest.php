613c15534b076ead69f7d8a2dc75fffeb35b68d6
Bruno Costa <bhcosta90@gmail.com>
2024-11-02 14:24:42 -0300

    wip

---
+++ tests/Feature/src/Core/UseCase/Category/DeleteCategoryUseCaseTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+use App\Models\Category;
+use Package\Application\Repository\CategoryRepository;
+use Package\Core\UseCase\Category\{DeleteCategoryUseCase};
+
+use function Pest\Laravel\{assertDatabaseCount, assertSoftDeleted};
+
+it('', function () {
+    // Arrange
+    $category   = Category::factory()->create();
+    $repository = new CategoryRepository(new Category());
+    $useCase    = new DeleteCategoryUseCase(categoryRepository: $repository);
+
+    // Act
+    $response = $useCase->handle($category->id);
+
+    // Assert
+    assertDatabaseCount('categories', 1);
+    assertSoftDeleted($category);
+});
